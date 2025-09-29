3ee192fc06e230a512b4b1bdf2fa0d4bbddebd90
YBlas <make202615@gmail.com>
2025-09-29 16:59:49 +0200

    axios install

--- index.ts
+++ index.ts
@@ -2,12 +2,21 @@
 
 
 
-const miVariable: string = "Hola guapes!";
+const promesaRickyMorty = fetch("https://rickandmortyapi.com/api/character/1");
 
-const niNumerito:number = 55;
+promesaRickyMorty
+  .then((response) => {
+    const data = response.json();
+    data.then((character) => {
+      console.log(character);
+    });
+  })
+  .catch((error) => {
+    console.log("Error en la petición", error);
+  })
+  .finally(() => {
+    console.log("Felicidades has conseguido un personaje o no.");
+  });
 
+console.log("¿Qué tal estais guapos?");
 
-
-console.log(niNumerito);
-
-console.log(miVariable);
